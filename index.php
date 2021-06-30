f5f6da60f65ffc5dea4c2b32e2b3f1a84b05a344
Antony <dufourantony2612@gmail.com>
2021-06-30 15:37:51 +0200

    test

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">sujet1</a>
+                            <a class="nav-link" href="sujet1">sujet1</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">sujet2</a>
