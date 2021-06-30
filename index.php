92c70486426159bdf15276cbe343411b94f1283b
Antony <dufourantony2612@gmail.com>
2021-06-30 14:43:12 +0200

    ajout du favicon

--- index.php
+++ index.php
@@ -7,11 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" type="image/jpg" href="favicon.ico"/>
     <title>Document</title>
 </head>
 
 <body>
-    <p class="h1 text-center">Super RSS Reader</p>
+    <p class="h1 text-center my-5">Super RSS Reader</p>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <div class="container-fluid d-flex flex-row">
