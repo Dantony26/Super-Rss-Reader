cdb77e158cf2bd081029afe7a9c362d41c0dbc99
Antony <dufourantony2612@gmail.com>
2021-06-30 10:48:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+$url = 'https://www.01net.com/rss/actualites/jeux/';
+$xml = simplexml_load_file($url);
+?>
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    foreach ($xml->channel->item as $item) {
+        echo $item->title;
+        echo $item->link;
+        echo $item->description;
+        echo date('D, d M Y',strtotime($item->pubDate));
+    }
+    ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+
+</html>
