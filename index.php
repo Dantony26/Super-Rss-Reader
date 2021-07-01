4d09c0683d774d320eec53a49680499010cf5405
Antony <dufourantony2612@gmail.com>
2021-07-01 09:26:55 +0200

    fix accueil navbar et condition d'affichage des articles

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="shortcut icon" type="image/jpg" href="favicon.ico" />
+    <link rel="shortcut icon" type="image/jpg" href="favicon.ico"/>
     <title>Document</title>
 </head>
 
@@ -16,7 +16,7 @@
     <nav class="navbar navbar-expand-lg <?= isset($_COOKIE['mode'])? 'navbar-light bg-light': 'navbar-dark bg-dark'; ?>">
         <div class="container">
             <div class="container-fluid d-flex flex-row">
-                <a class="navbar-brand" href="#">Accueil</a>
+                <a class="navbar-brand" href="/RSS">Accueil</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -72,8 +72,8 @@
         </div>
         <div>
             <?php
-            if (isset($_COOKIE['numberOfArticles'])) {
-                for ($i = 0; $i < $_COOKIE['numberOfArticles']; $i++) {
+            if (isset($emploiXML) && isset($entrepriseXML) && isset($industrieXML) && isset($economieFrancaiseXML) && isset($economieXML)) {
+                for ($i = 0; $i < $articlesNumber; $i++) {
                     $randomArticle = rand(0, 4);
                     $date = $articles[$randomArticle]->item[$i]->pubDate;
             ?>
@@ -101,8 +101,8 @@
                         <div class="col-11 col-md-8 text-vertical-align">
                             <p class="articleText"><?= $articles[$randomArticle]->item[$i]->title; ?></p>
                         </div>
-                        <div class="col-6 col-lg-1 articleText text-lg-center">
-                            <button type="button" class="btn btn-bg" data-bs-toggle="modal" data-bs-target="#modalArticle<?= $i ?>">
+                        <div class="col-6 col-lg-1 text-lg-center d-flex justify-content-center align-items-center">
+                            <button type="button" class="btn btn-bg articleText" data-bs-toggle="modal" data-bs-target="#modalArticle<?= $i ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-zoom-in" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z" />
                                     <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z" />
@@ -133,70 +133,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                <?php }
-            } else {
-                for ($i = 0; $i < 12; $i++) {
-                    $randomArticle = rand(0, 4);
-                    $date = $articles[$randomArticle]->item[$i]->pubDate;
-                ?>
-                    <div class="d-flex flex-row justify-content-between border my-3 p-3 row gx-0">
-                        <div class="col-1 square <?php switch ($randomArticle) {
-                                                        case 0:
-                                                            echo "bg-primary";
-                                                            break;
-                                                        case 1:
-                                                            echo "bg-success";
-                                                            break;
-                                                        case 2:
-                                                            echo "bg-danger";
-                                                            break;
-                                                        case 3:
-                                                            echo "bg-info";
-                                                            break;
-                                                        case 4:
-                                                            echo "bg-dark";
-                                                            break;
-                                                        default:
-                                                            echo "bg-secondary";
-                                                            break;
-                                                    } ?>"></div>
-                        <div class="col-11 col-md-8 text-vertical-align">
-                            <p class="articleText"><?= $articles[$randomArticle]->item[$i]->title; ?></p>
-                        </div>
-                        <div class="col-6 col-lg-1 articleText text-lg-center">
-                            <button type="button" class="btn btn-bg" data-bs-toggle="modal" data-bs-target="#modalArticle<?= $i ?>">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-zoom-in" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z" />
-                                    <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z" />
-                                    <path fill-rule="evenodd" d="M6.5 3a.5.5 0 0 1 .5.5V6h2.5a.5.5 0 0 1 0 1H7v2.5a.5.5 0 0 1-1 0V7H3.5a.5.5 0 0 1 0-1H6V3.5a.5.5 0 0 1 .5-.5z" />
-                                </svg>
-                            </button>
-                        </div>
-                        <div class="col-6 col-lg-2 text-vertical-align text-center">
-                            <a class="articleText text-decoration-none" href="<?= $articles[$randomArticle]->item[$i]->link; ?>">Lien vers l'article</a>
-                        </div>
-                    </div>
-                    <!-- Modal -->
-                    <div class="modal fade" id="modalArticle<?= $i ?>" tabindex="-1">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header d-flex flex-column">
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    <p> <?= strftime("%A %d %B %Y %H:%M:%S", strtotime($date)); ?></p>
-                                    <p class="modal-title h5" id="exampleModalLabel"><?= $articles[$randomArticle]->item[$i]->title; ?></p>
-                                </div>
-                                <div class="modal-body">
-                                    <img class="w-100 my-3" src="<?= $articles[$randomArticle]->item[$i]->children('media', true)->content->attributes(); ?>" alt="">
-                                    <p><?= $articles[$randomArticle]->item[$i]->description; ?></p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
-                                    <a href="<?= $articles[$randomArticle]->item[$i]->link; ?>"><button type="button" class="btn btn-primary">Aller ver l'article</button></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    </div>            
             <?php }
             }
 
